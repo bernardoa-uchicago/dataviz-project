45866c0a34dda17a876113d1196d6ea4b5342a46
Bernardo Albuquerque <bernardoa@uchicago.edu>
2026-03-10 23:51:20 -0500

    now

--- utils/io.py
+++ utils/io.py
@@ -12,6 +12,6 @@ def load_data():
     ICE_ex_individuals = pd.read_excel("data/ICE_data.xlsx", sheet_name='ICE T42 Expulsions Indivduals')
     ICE_ex_flights = pd.read_excel("data/ICE_data.xlsx", sheet_name='ICE T42 Expulsions Flights ')
     ICE_arrest_25 = pd.read_csv("data/ICE25.csv", encoding= "latin-1")
-    ICE_arrest_26 = pd.read_csv("data/ICE26(sheet1).csv", encoding= "latin-1")
+    ICE_arrest_26 = pd.read_csv("data/ICE26(Sheet1).csv", encoding= "latin-1")
     ICE_Countries = pd.read_csv("data/ICE_data(ICE Removals).csv", encoding = "latin-1")
     return ICE_atd, ICE_arrests, ICE_detentions, ICE_removals, ICE_ex_individuals, ICE_ex_flights, ICE_Countries, ICE_arrest_25, ICE_arrest_26 
